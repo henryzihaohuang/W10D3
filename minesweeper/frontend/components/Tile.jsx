fb861d360c3463abaebda915463920061558afe8
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-01-06 13:56:06 -0600

    Phase 3: Tile

--- minesweeper/frontend/components/Tile.jsx
+++ minesweeper/frontend/components/Tile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default class Tile extends React.Component {
     constructor(props){
         super(props)
@@ -7,8 +8,13 @@ export default class Tile extends React.Component {
 
 
     render(){
+        
         return(
-            <div>T</div>
+            <div>
+                {this.props.tile.bombed ? "☹" : this.props.tile.flagged ? "⚐" : this.props.tile.revealed ? `${this.props.tile.adjacentBombCount()}` : "T"}
+
+
+            </div>
         )
     }
 }
