64e62fef8408808f3b42dc28a7187b00b751c022
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-01-06 16:35:57 -0600

    Phase 4: A Working Game

--- minesweeper/frontend/components/Tile.jsx
+++ minesweeper/frontend/components/Tile.jsx
@@ -5,24 +5,35 @@ export default class Tile extends React.Component {
     constructor(props){
         super(props)
 
-        this.state = {revealed: this.props.tile.revealed}
+        this.state = {explored: this.props.tile.explored}
         this.handleClick = this.handleClick.bind(this)
     }
 
 
-    componentDidMount(){
-    }
-
     handleClick(event){
         event.preventDefault()
-        this.setState({revealed: !this.state.revealed})
+        this.setState({explored: !this.state.explored})
     }
 
-    render(){
-        const char = this.state.revealed ? this.props.tile.bombed ? "☹" : this.props.tile.flagged ? "⚐" : `${this.props.tile.adjacentBombCount()}` : ""
+    // componentDidMount (){
+    //     if (.explored && obj.adjacentBombCount() === 0) {
+                    
+    //             const coords = obj.neighbors();
+    //                 coords.map((coord) => {
+                        
+    //                     console.log(this.props.board.grid[coord[0]][coord[1]]);
 
+    //                     if (this.props.board.grid[coord[0]][coord[1]].adjacentBombCount()===0) {
+    //                         return <Tile tile={obj} updateGame={this.updateGame} board = {this.props.board} key={idx} className="tile explored"/>
+    //                     }
+    //              })
+    // }
+
+    render(){
+        const char = this.state.explored ? this.props.tile.bombed ? "☹" : this.props.tile.flagged ? "⚐" : `${this.props.tile.adjacentBombCount()}` : ""
+        
         return(
-            <div onClick={this.handleClick} className= {this.props.tile.bombed ? "tile bombed" : this.props.tile.flagged ? "tile flagged" : this.state.revealed ? `tile revealed` : "tile"}>
+            <div onClick={this.handleClick} className= {this.props.tile.bombed ? "tile bombed" : this.props.tile.flagged ? "tile flagged" : this.state.explored ? `tile explored` : "tile"}>
                 {char}
             </div>
         )
